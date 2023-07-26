bbd13d20af466e5c0d6767bd84b1b2216222b4fb
Yusuf Ali Selek <yusufaliselek41@gmail.com>
2023-07-26 21:20:53 +0300

    Update configApi.ts

--- client/src/api/configApi.ts
+++ client/src/api/configApi.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseURL = 'http://localhost:5000/';
+const baseURL = 'http://https://library-management-backend-wyt2.onrender.com/';
 export class ConfigApi {
     public static LibraryApi() {
         return axios.create({
